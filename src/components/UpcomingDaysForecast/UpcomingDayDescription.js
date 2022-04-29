00f89535390e5115f2b31c2e1351e70ecfbb8b5d
MirfayzDev <mirfayzisomiddinov23@gmail.com>
2022-04-30 00:12:18 +0500

    fixed upcomingdays weekday

--- src/components/UpcomingDaysForecast/UpcomingDayDescription.js
+++ src/components/UpcomingDaysForecast/UpcomingDayDescription.js
@@ -1,13 +1,17 @@
 import styles from './UpcomingDayDescription.module.css'
 
 function UpcomingDayDescription({data, index}) {
-    const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     const date = new Date()
+    let day;
+    if (date.getDay() >= 6) {
+        day = 0
+    }
 
     return (
         <li className={styles.weekday}>
             <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt="weather icon"/>
-            <span className={styles.date}>{weekdays[date.getDay() + index]}</span><br/>
+            <span className={styles.date}>{weekdays[day + index]}</span><br/>
             <span className={styles.temp}>{data.main.temp.toFixed(0)}&deg;</span>
         </li>
     )
