21499dedebd98fb570878b3dc4f7e15d5bfa4cbe
MirfayzDev <mirfayzisomiddinov23@gmail.com>
2022-05-18 08:21:37 +0500

    fixed date error and added some styles

--- src/components/UpcomingDaysForecast/UpcomingDayDescription.js
+++ src/components/UpcomingDaysForecast/UpcomingDayDescription.js
@@ -1,22 +1,24 @@
 import styles from './UpcomingDayDescription.module.css'
+import {useEffect} from "react";
 
 const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
 const date = new Date()
-let day;
+let day = date.getDay();
 
 function UpcomingDayDescription({data, index}) {
     const {main: {temp}, weather} = data
 
-    if (date.getDay() >= 6) {
-        day = 0
-    } else {
-        day = date.getDay() + 1
-    }
+    useEffect(() => {
+        return function () {
+            day = date.getDay()
+        }
+    }, [day])
 
     return (
         <li className={styles.weekday}>
             <img src={`http://openweathermap.org/img/wn/${weather[0]?.icon}.png`} alt="weather icon"/>
-            <span className={styles.date}>{weekdays[day + index]}</span><br/>
+            <span className={styles.date}>{weekdays[day = day + index <= 7 ? day + 1 : 0]}</span><br/>
             <span className={styles.temp}>{temp.toFixed(0)}&deg;</span>
         </li>
     )
